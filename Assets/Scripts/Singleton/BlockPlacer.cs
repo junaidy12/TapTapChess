841e9617b57f428f3f6e519a6dbbd04c8ae0a79b
junaidy12 <100924305+junaidy12@users.noreply.github.com>
2023-06-16 17:39:45 +0700

    Update BlockPlacer.cs

--- Assets/Scripts/Singleton/BlockPlacer.cs
+++ Assets/Scripts/Singleton/BlockPlacer.cs
@@ -44,82 +44,83 @@ public class BlockPlacer : MonoBehaviour
         if (!gameOver)
         {
             timer -= Time.deltaTime;
-
             if (timer < 0) gameOver = true;
-
-            if (Input.GetMouseButtonDown(0))
+            PlaceBlock()();
+        }
+    }
+    void PlaceBlock()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2Int gridPosition = LevelGrid.Instance.GetGridPosition(MouseWorld.GetMousePosition());
+            if (LevelGrid.Instance.SetGridObjectVisual(blockRandomizer.GetSelectedBlock(), gridPosition, false))
             {
-                Vector2Int gridPosition = LevelGrid.Instance.GetGridPosition(MouseWorld.GetMousePosition());
-                if (LevelGrid.Instance.SetGridObjectVisual(blockRandomizer.GetSelectedBlock(), gridPosition, false))
+
+                #region AttackPhase
+
+                List<Vector2Int> invalidPositionList = blockRandomizer.GetSelectedBlock().CheckUnvalidGridPosition(
+                    gridPosition, LevelGrid.Instance.GetGridSize());
+
+                int i = 0;
+                bool attack = false;
+
+                while (invalidPositionList.Count > i)
                 {
-
-                    #region AttackPhase
-
-                    List<Vector2Int> invalidPositionList = blockRandomizer.GetSelectedBlock().CheckUnvalidGridPosition(
-                        gridPosition, LevelGrid.Instance.GetGridSize());
-
-                    int i = 0;
-                    bool attack = false;
-
-                    while (invalidPositionList.Count > i)
+                    if (LevelGrid.Instance.GetGridObject(invalidPositionList[i]).block.blockType != Block.BlockType.Default)
                     {
-                        if (LevelGrid.Instance.GetGridObject(invalidPositionList[i]).block.blockType != Block.BlockType.Default)
-                        {
-                            attack = true;
-                            break;
-                        }
-                        i++;
+                        attack = true;
+                        break;
                     }
-
-
-                    for (int x = 0; x < invalidPositionList.Count; x++)
-                    {
-                        if (attack)
-                        {
-                            Block atkBlock = Instantiate(attackBlock,
-                                new Vector3(LevelGrid.Instance.GetWorldPosition(invalidPositionList[x]).x,
-                                LevelGrid.Instance.GetWorldPosition(invalidPositionList[x]).y,
-                                -.03f),
-                                Quaternion.identity, transform);
-
-                            if (LevelGrid.Instance.GetGridObject(invalidPositionList[x]).block.blockType != Block.BlockType.Default)
-                            {
-
-                                SpriteRenderer spriteRenderer = atkBlock.transform.GetComponentInChildren<SpriteRenderer>();
-                                spriteRenderer.color = new Color(1, 1, 1, 0.7f);
-
-                                gameOver = true;
-
-                            }
-                        }
-                    }
-
-                    #endregion
-
-                    //Lebih dari 3
-                    List<GridObject> gridObjects = LevelGrid.Instance.GetGridObjectSimilar(blockRandomizer.GetSelectedBlock());
-                    if (gridObjects.Count >= 3 && !gameOver)
-                    {
-                        foreach (GridObject blockObject in gridObjects)
-                        {
-                            blockObject.UpdateBlock(defaultBlock, false);
-                        }
-                    }
-
-                    score += blockRandomizer.GetSelectedBlock().score;
-                    blockRandomizer.RandomizeBlock();
-                    timer = placeTime;
+                    i++;
                 }
 
 
-                OnBlockClicked?.Invoke(this, new OnBlockClickedEventArgs
+                for (int x = 0; x < invalidPositionList.Count; x++)
                 {
-                    score = this.score
-                });
+                    if (attack)
+                    {
+                        Block atkBlock = Instantiate(attackBlock,
+                            new Vector3(LevelGrid.Instance.GetWorldPosition(invalidPositionList[x]).x,
+                            LevelGrid.Instance.GetWorldPosition(invalidPositionList[x]).y,
+                            -.03f),
+                            Quaternion.identity, transform);
+
+                        if (LevelGrid.Instance.GetGridObject(invalidPositionList[x]).block.blockType != Block.BlockType.Default)
+                        {
+
+                            SpriteRenderer spriteRenderer = atkBlock.transform.GetComponentInChildren<SpriteRenderer>();
+                            spriteRenderer.color = new Color(1, 1, 1, 0.7f);
+
+                            gameOver = true;
+
+                        }
+                    }
+                }
+
+                #endregion
+
+                //Lebih dari 3
+                List<GridObject> gridObjects = LevelGrid.Instance.GetGridObjectSimilar(blockRandomizer.GetSelectedBlock());
+                if (gridObjects.Count >= 3 && !gameOver)
+                {
+                    foreach (GridObject blockObject in gridObjects)
+                    {
+                        blockObject.UpdateBlock(defaultBlock, false);
+                    }
+                }
+
+                score += blockRandomizer.GetSelectedBlock().score;
+                blockRandomizer.RandomizeBlock();
+                timer = placeTime;
             }
+
+
+            OnBlockClicked?.Invoke(this, new OnBlockClickedEventArgs
+            {
+                score = this.score
+            });
         }
     }
-
     public void ResetBlockAndScore()
     {
         score = 0;
